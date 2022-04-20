fa608337fe024eb56ea32d266f0c7f51e8876d68
Alex Lane <97alane@gmail.com>
2022-04-19 21:13:40 -0500

    set up questions, need to make sure they are all working correctly before working on HTML genration

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const Engineer = require('./lib/engineer')
 const Intern = require('./lib/intern')
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateTeam = require('./src/generate_HTML');
+// const generateTeam = require('./src/generate_HTML');
 
 teams = [];
-const managerQuestions = () => {
-    inquirer.prompt([
+const generateManager = () => {
+   return inquirer.prompt([
     {
         type: 'input',
         name: 'name',
-        message: 'What is the name of the team managers name?',
+        message: 'What is the team managers name?',
     },
     {
         type: 'input',
@@ -33,30 +33,86 @@ const managerQuestions = () => {
 .then(managerInput => {
     const {name, email, officeNumber,id} = managerInput;
     const manager = new Manager(name, email, officeNumber, id)
-})
 
+    teamArray.push(manager); 
+    console.log(manager); 
+})
+};
+
+const generateEmployee = () => {
+   return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'type',
+            message: 'What type of team member are you adding?',
+            choices: ['engineer', 'intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the team member?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the email of the team member?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the id of the team member?',
+        },
+        
+        //add special inputs for intern and engineer
+        //if statments? when?
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is the engineers github?',
+            when: (input) => input.role === "Engineer",
+           
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is the interns school?',
+            when: (input) => input.role === "Intern",
+           
+        },
+        {
+        type: 'confirm',
+        name: 'addEmployee',
+        message: 'Would you like to add more team members?',
+    },
+
+    ])
+
+    .then(employeeData => {
+        let {name, type, email, id, github, school, addEmployee} = employeeData;
+        let employee;
+        if (type === "Engineer") {
+            employee = new Engineer (name, id, email, github);
+
+            console.log(employee);
+
+        } else if (type === "Intern") {
+            employee = new Intern (name, id, email, school);
+
+            console.log(employee);
+        }
+
+        teamArray.push(employee); 
+
+        if (confirmAddEmployee) {
+            return addEmployee(teamArray); 
+        } else {
+            return teamArray;
+        }
+    })
 };
 
 
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function(err){
-  
-        if (err) {
-            return console.log(err)
-        } else{
-            console.log("You did it!")
-        }
-    })
-    
-}
-function init() {
-    inquirer.prompt(managerQuestions)
-    .then(function(data){
-        writeToFile("index.html", generate_HTML(data));
-        
-    
-    })
-}
 
-// Function call to initialize app
-init();
+
+generateManager()
+.then(generateEmployee)
